de70e02f92f8556be010d38db7fe7aaf0af9958c
Chris Shepherd <chris@chrisshepherd.me>
2023-09-29 13:46:53 +0100

    Get unit testing working

--- premake5.lua
+++ premake5.lua
@@ -2,34 +2,43 @@ workspace "SpaceShooter"
    configurations { "Debug", "Release" }
    location "build"
 
+
+project "*"
+  cdialect "C17"
+  defines { "PLATFORM_DESKTOP" }
+  includedirs { "include" }
+  language "C"
+  links { "raylib" }
+  warnings "Extra"
+
+  filter "system:macosx"
+    kind "ConsoleApp"
+    libdirs { "libs/macos/**" }
+    links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework" }
+
+  filter "system:linux"
+    cdialect "gnu17"
+    kind "ConsoleApp"
+    libdirs { "libs/linux/**" }
+    links { "GL", "pthread", "dl", "m", "rt", "X11" }
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+
 project "SpaceShooter"
-   kind "WindowedApp"
-   language "C"
-   cdialect "C17"
-   targetdir "bin/%{cfg.buildcfg}"
+  files { "src/**.h", "src/**.c" }
+  kind "WindowedApp"
+  removefiles { "src/**_test.c" }
+  targetdir "bin/SpaceShooter/%{cfg.buildcfg}"
 
-   files { "src/**.h", "src/**.c" }
-   defines { "PLATFORM_DESKTOP" }
-
-   includedirs { "include" }
-   links { "raylib" }
-   warnings "Extra"
-
-   filter "system:macosx"
-      kind "ConsoleApp"
-      libdirs { "libs/macos/**" }
-      links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework" }
-
-   filter "system:linux"
-      kind "ConsoleApp"
-      cdialect "gnu17"
-      libdirs { "libs/linux/**" }
-      links { "GL", "pthread", "dl", "m", "rt", "X11" }
-
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+project "SpaceShooterTests"
+  defines { "TEST" }
+  files { "src/**.h", "src/**.c" }
+  kind "ConsoleApp"
+  removefiles { "src/main.c" }
+  targetdir "bin/SpaceShooterTests/%{cfg.buildcfg}"
---
+++ src/core/game_state_test.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+#include "utest/utest.h"
+
+UTEST(game_state, test_game_state) {
+    ASSERT_TRUE(true);
+}
